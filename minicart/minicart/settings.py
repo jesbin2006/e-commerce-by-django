9bad40c4f4358672871b8e2ab22d62151b15f14d
jesbin b joy <jesbinbijujoy@gmail.com>
2026-05-04 22:52:47 +0530

    deploy

--- minicart/minicart/settings.py
+++ minicart/minicart/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,6 +83,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'] = dj_database_url.parse("postgresql://jesbindb_user:GmGLVP98U1kqz0mb2NrHU31sRf888583@dpg-d7scmev7f7vs738463v0-a.ohio-postgres.render.com/jesbindb")
 
 
 # Password validation
